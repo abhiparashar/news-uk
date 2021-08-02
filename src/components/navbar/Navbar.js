661f6a6e2e1147f0aca7ddba6edf057ec0bdd64e
Abhishek Parashar <abhishekparashar@Abhisheks-MacBook-Pro.local>
2021-08-02 16:48:47 +0530

    search css implemented

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,11 +4,19 @@ import "./Navbar.css";
 import image from "../../assets/download.png";
 
 const Navbar = () => {
+  const clickHandler = (e)=>{
+    e.preventDefault()
+    console.log('abhishek');
+  }
   return (
     <div className="navbar">
       <Link to="/">
         <img src={image} alt="" />
       </Link>
+      <div className="search">
+        <input type="text" placeholder="search anything..."/>
+        <label onSubmit={clickHandler}><i class="fas fa-search"></i></label>
+      </div>
       <Link to="/logout" style={{textDecoration:"none"}}>
         <a>Logout</a>
       </Link>
