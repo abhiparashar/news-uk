c39c2bf7572ec3eb2e8eb84a1fe17d8559b74e04
Abhishek Parashar <abhishekparashar@Abhisheks-MacBook-Pro.local>
2021-08-02 11:42:11 +0530

    breakdown in pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
+import SignInPage from './pages/SigninPage/SignInPage'
+import SignUpPage from "./pages/signupPage/SignUpPage"
+import HomePage from "./pages/homePage/HomePage"
 
 function App() {
   return (
-    <div className="App">
-      ABHISHEK
+    <div className="app">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <SignInPage />
+          </Route>
+          <Route exact path="/signup">
+            <SignUpPage />
+          </Route>
+          <Route exact path="/news-uk.com">
+            <HomePage />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
