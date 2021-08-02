2bd2bcbf50dd0f9b0311a4ee4e0ff893d062d6a1
Abhishek Parashar <abhishekparashar@Abhisheks-MacBook-Pro.local>
2021-08-02 11:46:33 +0530

    breakdown components

---
+++ src/pages/SigninPage/SignInPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SignInPage.css'
+import SignIn from '../../components/signIn/SignIn'
+
+const SignInPage = () => {
+    return (
+        <div>
+            SignInPage
+        </div>
+    )
+}
+export default SignInPage
