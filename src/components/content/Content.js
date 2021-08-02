142f77b8d1c1f30755e47468e89e17bd56347c11
Abhishek Parashar <abhishekparashar@Abhisheks-MacBook-Pro.local>
2021-08-02 14:24:08 +0530

    api integrated

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,14 +1,34 @@
-import React from 'react'
-import './Content.css'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./Content.css";
 
 const Content = () => {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://newsapi.org/v2/top-headlines?country=us&apiKey=052ed8e1c35d4b92b95e0b7c53fbd30a")
+      .then((res) => setArticles(res.data.articles))
+      .catch((err) => console.log(err));
+  }, []);
+  const resultList = articles.map((article) => {
     return (
-        <div className="content">
-            
+      <div className="cards">
+        <div className="card">
+          <img src={article.urlToImage} alt="no IMAGE" />
+          <div className="published">{article.publishedAt}</div>
+          <div className="title">{article.title}</div>
+          <div className="description">{article.description}</div>
+          <div className="author">{article.author}</div>
+          <div className="seemore">{article.url}</div>
         </div>
-    )
-}
+      </div>
+    );
+  });
+  return (
+    <div className="content">
+      {resultList}
+    </div>
+  );
+};
 
-export default Content
-
-
+export default Content;
