f049ccde2bfcc49f4583908142ce865f27e401cd
Abhishek Parashar <abhishekparashar@Abhisheks-MacBook-Pro.local>
2021-08-03 10:22:02 +0530

    signup page design done

--- src/components/signUp/SignUp.js
+++ src/components/signUp/SignUp.js
@@ -1,55 +1,63 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import image from "../../assets/oliver-niblett-wh-7GeXxItI-unsplash.jpg";
 import "./SignUp.css";
 const SignUp = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const history = useHistory()
-  const submitHandler = async(e) =>{
-    e.preventDefault()
+  const history = useHistory();
+  const submitHandler = async (e) => {
+    e.preventDefault();
     try {
       const config = {
-        header:{
-          "Content-type":"application/json"
-        }
-      }
-      const {data} = await axios.post("http://localhost:5000/api/signup",{name,email,password},config)
-      localStorage.setItem('userinfo',JSON.stringify(data))
-      history.push("/")
+        header: {
+          "Content-type": "application/json",
+        },
+      };
+      const { data } = await axios.post(
+        "http://localhost:5000/api/signup",
+        { name, email, password },
+        config
+      );
+      localStorage.setItem("userinfo", JSON.stringify(data));
+      history.push("/");
     } catch (error) {
       console.log(error);
     }
-}
+  };
   return (
     <>
-      <form className="form" onSubmit={submitHandler}>
-      <label>Name</label>
-        <input
-          type="name"
-          placeholder="Type your name here"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <label>Email</label>
-        <input
-          type="email"
-          placeholder="Type your email here"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <label>Password</label>
-        <input
-          type="password"
-          placeholder="Type your password here"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button className="btn" type="submit">
-          Submit
-        </button>
-      </form>
+      <div className="signup">
+        <form className="form" onSubmit={submitHandler}>
+          <label className="label">Name</label>
+          <input
+            type="name"
+            placeholder="Type your name here"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <label className="label">Email</label>
+          <input
+            type="email"
+            placeholder="Type your email here"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label className="label">Password</label>
+          <input
+            type="password"
+            placeholder="Type your password here"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="btn" type="submit">
+            Submit
+          </button>
+        </form>
+        <img src={image} alt="" />
+      </div>
     </>
   );
 };
