00b6413df51d15451d30b7bfaed6497486bee9a8
Abhishek Parashar <abhishekparashar@Abhisheks-MacBook-Pro.local>
2021-08-03 10:50:53 +0530

    signin design done

--- src/components/signIn/SignIn.js
+++ src/components/signIn/SignIn.js
@@ -1,45 +1,55 @@
 import axios from "axios";
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-import './SignIn'
+import image from "../../assets/oliver-niblett-wh-7GeXxItI-unsplash.jpg";
+import "./SignIn.css";
 const SignIn = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const history = useHistory()
-    const submitHandler = async(e) =>{
-        e.preventDefault()
-        try {
-          const config = {
-            header:{
-              "Content-type":"application/json"
-            }
-          }
-          const {data} = await axios.post("http://localhost:5000/api/signin",{email,password},config)
-          localStorage.setItem('userinfo',JSON.stringify(data))
-          history.push("/")
-        } catch (error) {
-          console.log(error);
-        }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const config = {
+        header: {
+          "Content-type": "application/json",
+        },
+      };
+      const { data } = await axios.post(
+        "http://localhost:5000/api/signin",
+        { email, password },
+        config
+      );
+      localStorage.setItem("userinfo", JSON.stringify(data));
+      history.push("/");
+    } catch (error) {
+      console.log(error);
     }
+  };
   return (
     <>
-      <form className="form" onSubmit={submitHandler}>
-        <label>Email</label>
-        <input
-          type="email"
-          placeholder="Type your email here"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <label>Password</label>
-        <input
-          type="password"
-          placeholder="Type your password here"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button className="btn" type="submit">Submit</button>
-      </form>
+      <div className="signin">
+        <form className="form" onSubmit={submitHandler}>
+          <label>Email</label>
+          <input
+            type="email"
+            placeholder="Type your email here"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label className="label">Password</label>
+          <input
+            type="password"
+            placeholder="Type your password here"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="btn" type="submit">
+            Submit
+          </button>
+        </form>
+        <img src={image} alt="" />
+      </div>
     </>
   );
 };
